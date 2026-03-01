edc906f940254af66b7c07cd222a1bd64644044a
DESKTOP-RC33L5C\Cristhian <joan15arita@gmail.com>
2026-03-01 16:38:57 -0600

    -login

---
+++ ProyectoFinal/src/navigation/StackNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+//import HomeScreen from "../screens/HomeScreen";
+import TabsNavigator from './TabsNavigator';
+
+export type RootStackParamList = {
+    Login: undefined,
+    Tabs: {email: string},
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function StackNavigator() {
+    return(
+        <Stack.Navigator initialRouteName='Login' 
+        screenOptions={{headerShown: true}}
+        >
+            {/* <Stack.Screen 
+                name="Home"
+                component={HomeScreen}
+            /> */}
+            <Stack.Screen 
+                name="Login" 
+                component={LoginScreen} 
+                options={{title:'Inicio de Sesion'}}
+            />
+            <Stack.Screen 
+                name="Tabs"
+                component = {TabsNavigator}
+            />
+        </Stack.Navigator>
+    );
+}
